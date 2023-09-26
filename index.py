ee052a89e99d93f9ac3c8926fec1be3e5a346383
Hernand Azevedo <hernand.azevedo@zup.com.br>
2023-09-26 16:34:26 -0300

    chore: update

--- index.py
+++ index.py
@@ -1,20 +1,22 @@
 import argparse
 from pathlib import Path
-from typing import Any
-from lib import adjustPom
+from lib import adjustPom,Config
  
 argumentParser = argparse.ArgumentParser(description="Maven pom adjustments for publishing flutter modules on a maven repository ",
                                  formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 argumentParser.add_argument("-f", "--folder", help="Root folder to lookup for pom files", required=True)
 argumentParser.add_argument("-g", "--groupId", help="GroupId to replace on pom files", required=True)
 argumentParser.add_argument("-m", "--moduleName", help="ArtifactId of the main module to replace the artifact name", required=True)
-argumentParser.add_argument("-v", "--version", help="Version for the plugins", required=True)
+argumentParser.add_argument("-v", "--moduleVersion", help="Version for the module", required=True)
+argumentParser.add_argument("-p", "--pluginVersion", help="Version for the plugins", required=True)
 
 args = argumentParser.parse_args()
-config = vars(args)
+dictionary = vars(args)
 
-def main(config: dict[str, Any]):
-    for path in Path(config.get("folder")).rglob('*.pom'):
-        adjustPom(filename=str(path.absolute()), newGroupId=config.get("groupId"))
+#Example: python index.py --folder repo --groupId "com.sample3" --moduleName "mymodulename" --moduleVersion "0.0.1-rc1" --pluginVersion "0.0.1"
+def main(config: Config):
+    pomList: list[Path] = list(Path(config.folder).rglob('*.pom'))
+    for path in pomList:
+        adjustPom(pomFile=str(path.absolute()), pomList=pomList, config=config)
 
-main(config)
+main(Config(dictionary))
--- lib.py
+++ lib.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+from typing import Any
 import xml.etree.ElementTree as ElementTree
 
 NAMESPACE = 'http://maven.apache.org/POM/4.0.0'
@@ -7,17 +9,38 @@ DEPENDENCIES = PRE + "dependencies"
 DEPENDENCY = PRE + "dependency"
 VERSION = PRE + "version"
 ARTIFACT_ID = PRE + "artifactId"
+DEFAULT_MODULE_ID = "flutter_release"
+class Config:
+    def __init__(self, config: dict[str, Any]):
+        self.folder: str = config.get("folder")
+        self.groupId: str = config.get("groupId")
+        self.moduleName: str = config.get("moduleName")
+        self.moduleVersion: str = config.get("moduleVersion")
+        self.pluginVersion: str = config.get("pluginVersion")
 
-def getElementTree(filename: str) -> ElementTree:
+    def toString(self): return """Config(folder: {folder}, groupId: {groupId}, moduleName: {moduleName}, moduleVersion: {moduleVersion}, pluginVersion: {pluginVersion} )
+    """.format(folder = self.folder, groupId = self.groupId, moduleName = self.moduleName, moduleVersion = self.moduleVersion, pluginVersion = self.pluginVersion)   
+
+def getElementTree(pomFile: str) -> ElementTree:
     parser = ElementTree.XMLParser(target=ElementTree.TreeBuilder(insert_comments=True))
     ElementTree.register_namespace('', NAMESPACE) 
-    tree = ElementTree.parse(filename, parser)
+    tree = ElementTree.parse(pomFile, parser)
     return tree
 
-def adjustPom(filename: str, newGroupId: str) : 
-    print("Add newGroupId {newGroupId} filename: {filename}".format(filename = filename, newGroupId = newGroupId))
-    tree: ElementTree = getElementTree(filename=filename)
+def adjustPom(pomFile: str, pomList: list[Path], config: Config) : 
+    print("Parsing pomFile: {pomFile} \n with config: {config} ".format(pomFile = pomFile, config = config.toString()))
+    tree: ElementTree = getElementTree(pomFile=pomFile)
     root = tree.getroot()
-    grpId = root.find(GROUP_ID)
-    grpId.text = newGroupId
-    tree.write(filename +".tmp")
+    artifactId = groupId = root.find(ARTIFACT_ID)
+    version = root.find(VERSION)
+    if(artifactId.text == DEFAULT_MODULE_ID):    
+        version.text = config.moduleVersion
+        artifactId = root.find(ARTIFACT_ID)
+        artifactId.text = config.moduleName
+    else:
+        version.text = config.pluginVersion
+
+    groupId = root.find(GROUP_ID)
+    groupId.text = config.groupId
+    
+    tree.write(pomFile +".tmp")
