7447819c9b5020d0736bf1876100a25685ef170b
Hernand Azevedo <hernand.azevedo@zup.com.br>
2023-09-27 10:33:28 -0300

    chore: update code

--- index.py
+++ index.py
@@ -1,22 +1,33 @@
 import argparse
 from pathlib import Path
-from lib import adjustPom,Config
+from lib import Pom, adjustPom,Config, getElementTree
+import xml.etree.ElementTree as ET
+
  
 argumentParser = argparse.ArgumentParser(description="Maven pom adjustments for publishing flutter modules on a maven repository ",
                                  formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 argumentParser.add_argument("-f", "--folder", help="Root folder to lookup for pom files", required=True)
 argumentParser.add_argument("-g", "--groupId", help="GroupId to replace on pom files", required=True)
-argumentParser.add_argument("-m", "--moduleName", help="ArtifactId of the main module to replace the artifact name", required=True)
-argumentParser.add_argument("-v", "--moduleVersion", help="Version for the module", required=True)
-argumentParser.add_argument("-p", "--pluginVersion", help="Version for the plugins", required=True)
+argumentParser.add_argument("-mn", "--moduleName", help="ArtifactId of the main module to replace the artifact name", required=True)
+argumentParser.add_argument("-mv", "--moduleVersion", help="Version for the module", required=True)
+argumentParser.add_argument("-pv", "--pluginVersion", help="Version for the plugins", required=True)
+argumentParser.add_argument("-pg", "--pluginGroupId", help="GroupId for the plugins, if not present, defaults to groupdId")
 
 args = argumentParser.parse_args()
 dictionary = vars(args)
 
 #Example: python index.py --folder repo --groupId "com.sample3" --moduleName "mymodulename" --moduleVersion "0.0.1-rc1" --pluginVersion "0.0.1"
 def main(config: Config):
-    pomList: list[Path] = list(Path(config.folder).rglob('*.pom'))
-    for path in pomList:
-        adjustPom(pomFile=str(path.absolute()), pomList=pomList, config=config)
+    pomList: list[Pom] = list()
+    for path in Path(config.folder).rglob('*.pom'):
+        pomFile = str(path.absolute())
+        pomList.append(Pom(tree=getElementTree(pomFile=pomFile), pomFile=pomFile))
+
+    for pom in pomList:    
+        adjustPom(pom=pom, pomList=pomList, config=config)
+
+    #Writing trees on respective files
+    for pom in pomList:    
+            pom.tree.write(pom.pomFile +".tmp")
 
 main(Config(dictionary))
--- lib.py
+++ lib.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 from typing import Any
-import xml.etree.ElementTree as ElementTree
+import xml.etree.ElementTree as ET
 
 NAMESPACE = 'http://maven.apache.org/POM/4.0.0'
 PRE = "{" + NAMESPACE +"}"
@@ -10,37 +10,58 @@ DEPENDENCY = PRE + "dependency"
 VERSION = PRE + "version"
 ARTIFACT_ID = PRE + "artifactId"
 DEFAULT_MODULE_ID = "flutter_release"
+
+class Pom:
+    def __init__(self, pomFile: str, tree: ET.ElementTree):
+        self.pomFile = pomFile
+        self.tree = tree
 class Config:
     def __init__(self, config: dict[str, Any]):
-        self.folder: str = config.get("folder")
-        self.groupId: str = config.get("groupId")
-        self.moduleName: str = config.get("moduleName")
-        self.moduleVersion: str = config.get("moduleVersion")
-        self.pluginVersion: str = config.get("pluginVersion")
+        self.folder: str = str(config.get("folder"))
+        self.groupId: str = str(config.get("groupId"))
+        self.moduleName: str = str(config.get("moduleName"))
+        self.moduleVersion: str = str(config.get("moduleVersion"))
+        self.pluginVersion: str = str(config.get("pluginVersion"))
+        self.pluginGroupId: str = str(config.get("pluginGroupId") or config.get("groupId"))
 
-    def toString(self): return """Config(folder: {folder}, groupId: {groupId}, moduleName: {moduleName}, moduleVersion: {moduleVersion}, pluginVersion: {pluginVersion} )
-    """.format(folder = self.folder, groupId = self.groupId, moduleName = self.moduleName, moduleVersion = self.moduleVersion, pluginVersion = self.pluginVersion)   
+    def toString(self): return """Config(folder: {folder}, groupId: {groupId}, moduleName: {moduleName}, moduleVersion: {moduleVersion}, pluginVersion: {pluginVersion}, pluginGroupId: {pluginGroupId} )
+    """.format(folder = self.folder, groupId = self.groupId, moduleName = self.moduleName, moduleVersion = self.moduleVersion, pluginVersion = self.pluginVersion, pluginGroupId = self.pluginGroupId)   
 
-def getElementTree(pomFile: str) -> ElementTree:
-    parser = ElementTree.XMLParser(target=ElementTree.TreeBuilder(insert_comments=True))
-    ElementTree.register_namespace('', NAMESPACE) 
-    tree = ElementTree.parse(pomFile, parser)
+def getElementTree(pomFile: str) -> ET.ElementTree:
+    parser = ET.XMLParser(target=ET.TreeBuilder(insert_comments=True))
+    ET.register_namespace('', NAMESPACE) 
+    tree = ET.parse(pomFile, parser)
     return tree
 
-def adjustPom(pomFile: str, pomList: list[Path], config: Config) : 
-    print("Parsing pomFile: {pomFile} \n with config: {config} ".format(pomFile = pomFile, config = config.toString()))
-    tree: ElementTree = getElementTree(pomFile=pomFile)
-    root = tree.getroot()
-    artifactId = groupId = root.find(ARTIFACT_ID)
+def adjustPomDependencies(artifactId: str, pomList: list[Pom], config: Config) : 
+    print("adjustPomDependencies artifactId: {artifactId} \n with config: {config} ".format(artifactId = artifactId.text, config = config.toString()))
+    
+    for pom in pomList:
+        root = pom.tree.getroot()
+        dependencies = root.find(DEPENDENCIES)
+        if(dependencies != None):
+            for dependency in dependencies:
+                depArtifactId = dependency.find(ARTIFACT_ID)
+                depVersion = dependency.find(VERSION)
+                depGroupId = dependency.find(GROUP_ID)
+                if(depArtifactId.text == artifactId.text):    
+                    depVersion.text = config.pluginVersion
+                    depGroupId.text = config.pluginGroupId
+
+
+def adjustPom(pom: Pom, pomList: list[Pom], config: Config) : 
+    print("adjustPom pomFile: {pomFile} \n with config: {config} ".format(pomFile = pom.pomFile, config = config.toString()))
+    root = pom.tree.getroot()
+    artifactId = root.find(ARTIFACT_ID)
     version = root.find(VERSION)
+    
+    groupId = root.find(GROUP_ID)
+    groupId.text = config.groupId
+    
     if(artifactId.text == DEFAULT_MODULE_ID):    
         version.text = config.moduleVersion
         artifactId = root.find(ARTIFACT_ID)
         artifactId.text = config.moduleName
     else:
         version.text = config.pluginVersion
-
-    groupId = root.find(GROUP_ID)
-    groupId.text = config.groupId
-    
-    tree.write(pomFile +".tmp")
+        adjustPomDependencies(artifactId=artifactId, pomList=pomList, config=config)
